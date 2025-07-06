658b6468b8f79e6fc24287a3f66bb975053378cd
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-07-06 20:08:09 +0100

    Created users, workouts, and payments apps and added them to INSTALLED_APPS

--- gainsgalore/settings.py
+++ gainsgalore/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our custom apps
+    'users',
+    'workouts',
+    'payments',
 ]
 
 MIDDLEWARE = [
