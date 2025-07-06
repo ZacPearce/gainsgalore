c07352aff0edddbf90849b9394ead494dffc05d7
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-07-06 21:20:56 +0100

    Added user dashboard with login protection and a simple homepage view

--- gainsgalore/urls.py
+++ gainsgalore/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("""
+        <h1>Welcome to Gains Galore 💪🏾</h1>
+        <p>
+            <a href='/users/login/'>Login</a> |
+            <a href='/users/register/'>Register</a>
+        </p>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', home_view, name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm, CustomLoginForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  # we'll make this later
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = CustomLoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
