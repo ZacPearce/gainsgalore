d6abefa9a003eb6f36fc7b00e63ce9dafb1832ac
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-07-07 23:42:55 +0100

    Wrapped home_view in try/except for Render error debugging

--- gainsgalore/urls.py
+++ gainsgalore/urls.py
@@ -4,12 +4,14 @@ from django.shortcuts import render
 from workouts.models import WorkoutPlan
 
 def home_view(request):
-    if request.user.is_authenticated:
-        workouts = WorkoutPlan.objects.all()
-    else:
-        workouts = WorkoutPlan.objects.filter(is_premium=False)
-
-    return render(request, 'home.html', {'workouts': workouts})
+    try:
+        if request.user.is_authenticated:
+            workouts = WorkoutPlan.objects.all()
+        else:
+            workouts = WorkoutPlan.objects.filter(is_premium=False)
+        return render(request, 'home.html', {'workouts': workouts})
+    except Exception as e:
+        return render(request, 'error.html', {'error': str(e)})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
